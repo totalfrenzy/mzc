4ddebf879b33bd51508b3431b2057e4b51d395b7
John Murphy <John Murphy>
2016-04-27 23:17:07 +1000

    Have building abilities on spawn Fixed issues with OnPlayerPickHero

--- game/dota_addons/mzc/scripts/vscripts/gamemode.lua
+++ game/dota_addons/mzc/scripts/vscripts/gamemode.lua
@@ -16,6 +16,7 @@ function CustomGameMode:InitGameMode()
 	-- Event Hooks
 	ListenToGameEvent('entity_killed', Dynamic_Wrap(CustomGameMode, 'OnEntityKilled'), self)
 	ListenToGameEvent('dota_player_pick_hero', Dynamic_Wrap(CustomGameMode, 'OnPlayerPickHero'), self)
+	--ListenToGameEvent('npc_spawned', Dynamic_Wrap(CustomGameMode, 'OnUnitSpawned'), self)
 
 	-- Filters
     GameRules:GetGameModeEntity():SetExecuteOrderFilter( Dynamic_Wrap( CustomGameMode, "FilterExecuteOrder" ), self )
@@ -33,6 +34,18 @@ function CustomGameMode:InitGameMode()
   	GameRules.ItemKV = LoadKeyValues("scripts/npc/npc_items_custom.txt")
   	GameRules.Requirements = LoadKeyValues("scripts/kv/tech_tree.kv")
 
+    GameRules:SetHeroRespawnEnabled(false)
+    GameRules:SetSameHeroSelectionEnabled(true)
+    GameRules:SetPostGameTime(100)
+    GameRules:SetPreGameTime(0)
+    GameRules:SetHeroSelectionTime(0)
+    GameRules:SetGoldPerTick(0)
+    GameRules:GetGameModeEntity():SetGoldSoundDisabled(true)
+    GameRules:GetGameModeEntity():SetAnnouncerDisabled(true)
+    GameRules:GetGameModeEntity():SetHUDVisible(DOTA_HUD_VISIBILITY_TOP_SCOREBOARD, false)
+
+	base_game_mode:SetCustomGameForceHero("npc_Dota_hero_kunkka")
+  	
   	-- Store and update selected units of each pID
 	GameRules.SELECTED_UNITS = {}
 
@@ -41,6 +54,125 @@ function CustomGameMode:InitGameMode()
 end
 
 -- A player picked a hero
+function CustomGameMode:OnUnitSpawned(keys)
+	local unit = EntIndexToHScript(keys.entindex)
+
+    if unit:IsRealHero() then
+        local hero = unit
+        local playerID = hero:GetPlayerID()
+
+        CustomGameMode:OnHeroInGame(hero)
+
+    else
+        local unitName = unit:GetUnitName()
+        if unitName and unitName ~= "" and not NPC_UNITS_CUSTOM[unitName] then
+            Log:warn("A non-custom unit was spawned! "..unitName)
+            unit:RemoveSelf()
+        end
+    end
+end
+
+function CustomGameMode:OnHeroInGame(hero)
+    local playerID = hero:GetPlayerID()
+
+    --if playerID == -1 then return end
+    --if GetPlayerData(playerID) then --Don't create playerdata twice
+     --   CustomGameMode:InitializeHero(playerID, hero) --INITIALIZE HERO
+     --   return
+    --end
+
+    local playerData = CreateDataForPlayer(playerID)
+    playerData.name = PlayerResource:GetPlayerName(playerID)
+
+    if playerData.name == "" then -- This normally happens in dev tools
+        playerData.name = 'Developer'
+    end
+
+    local teamID = PlayerResource:GetTeam(playerID)
+    --if COOP_MAP then
+        -- Player based colors
+    --    PlayerResource:SetCustomPlayerColor(playerID, PlayerColors[playerID][1], PlayerColors[playerID][2], PlayerColors[playerID][3])
+    --else
+        -- Team location based colors
+    --    PlayerResource:SetCustomPlayerColor(playerID, m_TeamColors[teamID][1], m_TeamColors[teamID][2], m_TeamColors[teamID][3])
+    --end
+
+    --playerData.sector = TEAM_TO_SECTOR[hero:GetTeamNumber()]
+
+    self:InitializeHero(playerID, hero)
+    self.playerSpawnIndexes[playerID] = playerData.sector + 1
+    self.availableSpawnIndex = self.availableSpawnIndex + 1
+
+    -- we must create the Elemental Summoner for this player
+    --local sector = playerData.sector + 1
+    --local summoner = CreateUnitByName("elemental_summoner", ElementalSummonerLocations[sector], false, nil, nil, hero:GetTeamNumber()) 
+    --summoner:SetOwner(hero)
+    --summoner:SetControllableByPlayer(playerID, true)
+    --summoner:SetAngles(0, 270, 0)
+    --summoner:AddItem(CreateItem("item_buy_pure_essence_disabled", nil, nil))
+    --summoner.icon = CreateUnitByName("elemental_summoner_icon", ElementalSummonerLocations[sector], false, nil, nil, hero:GetTeamNumber())
+    --playerData.summoner = summoner
+
+    hero:SetBaseMaxHealth(playerData.health)
+    hero:SetHealth(playerData.health)
+
+    hero:ModifyGold(0)
+    ModifyLumber(playerID, 0)  -- updates summoner spells
+
+end
+
+function GetPlayerData(playerID)
+	return PlayerData[playerID]
+end
+
+function CustomGameMode:InitializeHero(playerID, hero)
+    Log:info("InitializeHero "..playerID..":"..hero:GetUnitName())
+    hero:AddNewModifier(nil, nil, "modifier_disarmed", {})
+    hero:AddNewModifier(nil, nil, "modifier_attack_immune", {})
+    hero:AddNewModifier(hero, nil, "modifier_max_ms", {ms=GameSettings:GetMapSetting("BuilderMoveSpeed")})
+    --hero:AddNewModifier(hero, nil, "modifier_client_convars", {})
+    local playerData = hero:GetPlayerID()
+    --local playerData = GetPlayerData(playerID)
+
+    --Timers(0.03, function() 
+    --    hero:SetAbilityPoints(playerData.lumber or 0)
+    --    SetCustomGold(playerID, playerData.gold)
+    --end)
+
+    -- Give building items
+    hero:AddItem(CreateItem("item_build_arrow_tower", hero, hero))
+    hero:AddItem(CreateItem("item_build_cannon_tower", hero, hero))
+    hero:AddItem(CreateItem("item_build_periodic_tower_disabled", hero, hero))
+    hero:AddAbility("tower")
+    hero:AddAbility("dragon_tower")
+
+    if not playerData.toggle_grid_item then
+        playerData.toggle_grid_item = hero:AddItem(CreateItem("item_toggle_grid", hero, hero))
+        playerData.toggle_grid_item.particles = setmetatable({}, {
+            __index = (function(tab, index)
+                tab[index] = {}
+                return tab[index]
+            end)
+        })
+    elseif IsValidEntity(playerData.toggle_grid_item) and playerData.toggle_grid_item_old then
+        Timers(0.03, function()
+            local item = hero:AddItem(playerData.toggle_grid_item_old)
+            item:SetPurchaser(hero)
+        end)
+    end
+    
+    Timers:CreateTimer(0.1, function() hero:SwapItems(3, 5) end)
+
+    -- Additional Heroes UI
+    heroUI = hero:FindAbilityByName("hero_ui")
+    if heroUI then
+        heroUI:SetLevel(1)
+    end
+
+    UpdatePlayerSpells(playerID)
+    UpdateScoreboard(playerID)
+end
+
 function CustomGameMode:OnPlayerPickHero(keys) --keys
 
 	local hero = EntIndexToHScript(keys.heroindex)
@@ -63,9 +195,9 @@ function CustomGameMode:OnPlayerPickHero(keys) --keys
 	--building:SetHealth(building:GetMaxHealth()/3)
 
 	-- These are required for repair to know how many resources the building takes
-	building.GoldCost = 100
-	building.LumberCost = 100
-	building.BuildTime = 15
+	--building.GoldCost = 100
+	--building.LumberCost = 100
+	--building.BuildTime = 15
 
 	-- Add the building to the player structures list
 	--player.buildings[city_center_name] = 1
@@ -102,18 +234,18 @@ function CustomGameMode:OnPlayerPickHero(keys) --keys
 	hero:SetGold(10, false)
 	ModifyLumber(player, 10)
 
-	GameMode:SetFogOfWarDisabled(true) 
+	--GameMode:SetFogOfWarDisabled(true) 
 
 
 	-- Lumber tick
-	Timers:CreateTimer(1, function()
-		ModifyLumber(player, 10)
-		return 10
-	end)
+	--Timers:CreateTimer(1, function()
+	--	ModifyLumber(player, 10)
+	--	return 10
+	--end)
 
 	-- Give a building ability
-	local item = CreateItem("item_build_wall", hero, hero)
-	hero:AddItem(item)
+	--local item = CreateItem("item_build_wall", hero, hero)
+	--hero:AddItem(item)
 
 	-- Learn all abilities (this isn't necessary on creatures)
 	for i=0,15 do
